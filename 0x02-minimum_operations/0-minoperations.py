93f47ed869749b32b471ce9fcaf7ff6e9d0ed823
StephanieMfon <okpomfonstephanie@gmail.com>
2024-05-09 17:54:48 +0100

    worked on minimum operations file

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""Function that calculates the minimum number 
+of operations needed to print n 'H' in a file"""
+
+
+def minOperations(n):
+    """Calculates the minimum operations to result in n"""
+    process = 2
+    operations = 0
+    while n > 1:
+
+        while n % process == 0:
+            operations += process
+            n /= process
+        process += 1
+    return operations
---
+++ 0x02-minimum_operations/README.md
@@ -0,0 +1,1 @@
+A repository with the solutin to ALX's min operations interview question.
