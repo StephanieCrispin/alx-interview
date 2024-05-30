9ac6f6875e3ea62d2d35706e610b35005565080e
StephanieMfon <okpomfonstephanie@gmail.com>
2024-05-30 23:08:42 +0100

    trial

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -11,10 +11,10 @@ def backtrack(r, n, cols, pos, neg, board):
     """
     if r == n:
         res = []
-        for l in range(len(board)):
-            for k in range(len(board[l])):
-                if board[l][k] == 1:
-                    res.append([l, k])
+        for le in range(len(board)):
+            for k in range(len(board[le])):
+                if board[le][k] == 1:
+                    res.append([le, k])
         print(res)
         return
 
