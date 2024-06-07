54b00d0d44b447c8247df20f704452ed1bf68d62
StephanieMfon <okpomfonstephanie@gmail.com>
2024-06-07 03:01:48 +0100

    added files for starwars API

---
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -0,0 +1,23 @@
+const request = require("request");
+const movieId = process.argv[2];
+
+const options = {
+  url: `https://swapi-api.hbtn.io/api/films/${movieId}`,
+  method: "GET",
+};
+
+request(options, function (error, response, body) {
+  if (!error) {
+    const characters = JSON.parse(body).characters;
+    printCharacters(characters, 0);
+  }
+});
+
+const printCharacters = (characters, index) => {
+  request(characters[index], function (error, response, data) {
+    if (!error) {
+      console.log(JSON.parse(data).name);
+      if (index + 1 < characters.length) printCharacters(characters, index + 1);
+    }
+  });
+};
---
+++ 0x06-starwars_api/README.md
@@ -0,0 +1,1 @@
+A Readme for Starwars API Task
