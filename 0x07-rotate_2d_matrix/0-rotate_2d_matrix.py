5c2ea1c86db2c664fb5c90aab9f672e44fe0f03f
StephanieMfon <okpomfonstephanie@gmail.com>
2024-06-13 23:37:41 +0100

    added 2d matrix files

---
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""Contains function that rotates 2d matrix"""
+
+
+def rotate_2d_matrix(matrix):
+    """Function to rotate 2d matrix"""
+    arr1 = list(zip(*matrix))
+    return [list(single[::-1]) for single in arr1]
---
+++ 0x07-rotate_2d_matrix/README.md
@@ -0,0 +1,1 @@
+A readme for a single interview task to rotate a 2D Matrix, quite fun I must say
